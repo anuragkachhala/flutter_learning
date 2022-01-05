50d3a1eea04e1b37096c6c2b8105881b6318d5dd
anurag_kachhala <kachhalaanurag@gmail.com>
2022-01-05 16:17:54 +0530

    custom widget method

--- lib/quoteListUI.dart
+++ lib/quoteListUI.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'Quote.dart';
+
 void main() {
   runApp(const MaterialApp(
     home: QuoteListApp(),
@@ -20,6 +22,31 @@ class _QuoteListState extends State<QuoteListApp> {
     Quote('second quote ', 'second author'),
   ];
 
+  Widget quoteWidget(quote) {
+    return Card(
+      margin: EdgeInsets.all(16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.quote,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14.0, color: Colors.grey[400]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -31,7 +58,7 @@ class _QuoteListState extends State<QuoteListApp> {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => Text('${quote.quote} - ${quote.author} ')).toList(),
+        children: quotes.map((quote) => quoteWidget(quote)).toList(),
       ),
     );
   }
