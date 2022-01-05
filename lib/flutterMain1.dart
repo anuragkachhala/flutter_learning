c2a68ed87f7aa4ed8ec55129bc26af77fb62dd53
anurag_kachhala <kachhalaanurag@gmail.com>
2022-01-05 14:42:14 +0530

    row add

--- lib/flutterMain1.dart
+++ lib/flutterMain1.dart
@@ -18,16 +18,20 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: const Center(
-        child: Text('Hello anurag',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey,
-              fontFamily: 'TheNautigal'
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text('row'),
+          Container(
+            color: Colors.grey,
+            child: Text('its container'),
           ),
-        ),
+          Container(
+            color: Colors.blue,
+            child: Text('Blue Text'),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.amber,
